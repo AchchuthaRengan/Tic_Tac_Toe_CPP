90ab1dbf7a35b68cf2a06e05d96b03e227213711
Achchutha_Rengan <63031750+AchchuthaRengan@users.noreply.github.com>
2020-11-01 16:16:17 +0530

    Tic Tac Toe By Achchutha

---
+++ ttt.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+#include "ttt.hpp"
+
+	std::string board[9] = {" "," "," "," "," "," "," "," "," "};
+	int player = 1;
+	int position = 0;
+	
+	void introduction()
+	{
+		std::cout << "Press [Enter] to Begin: \n";
+		std::cin.ignore();
+		
+		std::cout << "=============== \n";
+		std::cout << "TIC TAC TOE \n";
+		std::cout << "=============== \n\n";
+		
+		std::cout << "Player 1 -- > X \n";
+		std::cout << "Player 2 -- > O \n\n";
+		
+		std::cout << "   1   |   2   |   3   \n";
+		std::cout << "-----------------------\n";
+		std::cout << "   4   |   5   |   6   \n";
+		std::cout << "-----------------------\n";
+		std::cout << "   7   |   8   |   9   \n\n";
+
+		std::cout << "Press corresponding numbers for corresponding places.\n\n";
+		
+	}
+	
+	bool is_winner()
+	{
+		bool winner = false;
+		
+		// checking rows
+		if((board[0] == board[1]) && (board[1] == board[2]) && (board[0] != " "))
+			winner = true;
+		else if((board[3] == board[4]) && (board[4] == board[5]) && (board[3] != " "))
+			winner = true;
+		else if((board[6] == board[7]) && (board[7] == board[8]) && (board[6] != " "))
+			winner = true;
+		
+		// columns
+		else if((board[0] == board[3]) && (board[3] == board[6]) && (board[0] != " "))
+			winner = true;
+		else if((board[1] == board[4]) && (board[4] == board[7]) && (board[1] != " "))
+			winner = true;
+		else if((board[2] == board[5]) && (board[5] == board[8]) && (board[2] != " "))
+			winner = true;
+		
+		//diagonals
+		else if((board[0] == board[4]) && (board[4] == board[8]) && (board[0] != " "))
+			winner = true;
+		else if((board[2] == board[4]) && (board[4] == board[6]) && (board[2] != " "))
+			winner = true;
+		
+		return winner;
+		
+	}
+	
+	bool is_filled()
+	{
+		bool filled = true;
+		for(int i = 0 ; i < 9; i++)
+		{
+			if(board[i] == " ")
+			{
+				filled = false;
+			}
+		}
+		return filled;
+	}
+	
+	void draw()
+	{
+		std::cout << "     |     |      \n";
+
+		std::cout << "  " << board[0] << "  |  " << board[1] << "  |  " << board[2] << "\n";
+
+		std::cout << "_____|_____|_____ \n";
+		std::cout << "     |     |      \n";
+
+		std::cout << "  " << board[3] << "  |  " << board[4] << "  |  " << board[5] << "\n";
+
+		std::cout << "_____|_____|_____ \n";
+		std::cout << "     |     |      \n";
+
+		std::cout << "  " << board[6] << "  |  " << board[7] << "  |  " << board[8] << "\n";
+		std::cout << "     |     |      \n";
+
+		std::cout << "\n";
+	}
+	
+	void set_position()
+	{
+		std::cout << "Player" << player << "'s turn enter between 1-9: \n";
+		
+		while(!(std::cin >> position))
+		{
+			std::cout << "Player" << player << " please enter between 1 to 9. \n" ;
+			std::cin.clear();
+			std::cin.ignore();
+		}
+		
+		std::cout << "\n";
+		
+		while(board[position-1] != " ")
+		{
+			std::cout << "Player" << " Oops!. It's already occupied.. Please enter another location. \n";
+			std::cout << "Enter number between 1-9: \n";
+			std::cin >> position;
+		}
+		
+		std::cout << "\n";
+	}
+	
+	void update_position()
+	{
+		if(player % 2 == 1)
+		{
+			board[position - 1] = "X";
+		}
+		else {
+			board[position - 1] = "O";
+		}
+	}
+	
+	void change_player()
+	{
+		if(player == 1)
+		{
+			player++;
+		}
+		else 
+		{
+			player--;
+		}
+	}
+	
+	void take_turn()
+	{
+		while(!is_winner() && !is_filled())
+		{
+			set_position();
+			
+			update_position();
+			
+			change_player();
+			
+			draw();
+		}
+	}
+	
+	void end_game()
+	{
+		if(is_winner())
+		{
+			std::cout << "There is a Winner! \n";
+		}
+		else if(is_filled())
+		{
+			std::cout << "It's a TIE. \n";
+		}
+	}
+	
---
+++ ttt.hpp
@@ -0,0 +1,9 @@
+bool is_winner();
+bool is_filled();
+void introduction();
+void take_turn();
+void set_position();
+void update_position();
+void change_player();
+void draw();
+void end_game();
